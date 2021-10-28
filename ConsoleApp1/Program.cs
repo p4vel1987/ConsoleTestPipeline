81dbfe1d23e5e4de27dcd706a7b453556fecee2b
Giovanni Appugliese <giovanni.appugliese@dabsi.it>
2021-10-28 12:43:43 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Auto a = new Auto($"marca {i}", $"tipo {i}");
+                Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(a));
+            }
+        }
+    }
+
+    public class Auto
+    {
+        public string Marca { get; set; }
+        public string Modello { get; set; }
+
+        public Auto(string marca, string modello)
+        {
+            Marca = marca;
+            Modello = modello;
+        }
+    }
+}
